33defe2b325e8782676303b20cd1aba9aeb96381
Sergey Nikitin <sergey@bitior.com>
2022-06-25 03:05:04 -0700

    Added fundLoan()

--- contracts/NFTLender.sol
+++ contracts/NFTLender.sol
@@ -1,40 +1,70 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "hardhat/console.sol";
 
 struct Loan {
-    uint256 loanId;
     ERC721 ntfToken;
     uint256 nftTokenId;
     uint256 price;
+    uint256 amount;
+    uint256 maturity;
+    address borrower;
+    uint8 status;
 }
 
 contract NFTLender {
+    uint8 constant STATUS_STARTED = 1;
+    uint8 constant STATUS_FUNDED = 2;
+    uint8 constant STATUS_REPAID = 3;
+    uint8 constant STATUS_LIQUIDATED = 4;
+
+    uint8 constant MAX_RATIO = 70;
+
     uint256 loanNumber = 0;
     mapping (uint256 => Loan) public loans;
+    ERC20 usdcToken;
 
-    constructor() {
+    constructor(ERC20 _usdcToken) {
         console.log("NFTLender constructor");
+        usdcToken = _usdcToken;
     }
 
-    function startLoan(ERC721 ntfToken, uint256 nftTokenId, uint256 price) public {
-        console.log("startLoan, loanNumber: %d", loanNumber);
+    function startLoan(ERC721 ntfToken, uint256 nftTokenId, uint256 price, uint256 maturity) public {
+        console.log("startLoan, loanId: %d", loanNumber);
         console.log("msg.sender: %s", msg.sender);
         console.log("address(this): %s", address(this));
 
-        ntfToken.transferFrom(msg.sender, address(this), nftTokenId);
-
         Loan memory loan = Loan({
-            loanId: loanNumber,
             ntfToken: ntfToken,
             nftTokenId: nftTokenId,
-            price: price
+            price: price,
+            amount: 0,
+            maturity: maturity,
+            borrower: msg.sender,
+            status: STATUS_STARTED
         });
 
         loans[loanNumber] = loan;
 
         loanNumber++;
+
+        ntfToken.transferFrom(msg.sender, address(this), nftTokenId);
+    }
+
+    function fundLoan(uint256 loanId, uint256 amount) public {
+        console.log("fundLoan, loanId: %d", loanId);
+        console.log("msg.sender: %s", msg.sender);
+        console.log("status: %d", loans[loanId].status);
+
+        require(loans[loanId].status == STATUS_STARTED, "The specified loan cannot be funded");
+        require(amount <= (loans[loanId].price * MAX_RATIO) / 100, "The loan amount cannot exceed 70% of the NFT price");
+
+        loans[loanId].amount = amount;
+        loans[loanId].status = STATUS_FUNDED;
+
+        usdcToken.transferFrom(msg.sender, loans[loanId].borrower, amount);
     }
 }
--- test/sample-test.js
+++ test/sample-test.js
@@ -2,10 +2,19 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 var nftLender;
+var usdc;
 var testNft;
 
 describe("NFTLender", function () {
-  it("Should create a test NFT token", async function () {
+  it("Create test USDC", async function () {
+    const Usdc = await ethers.getContractFactory("USDC");
+    usdc = await Usdc.deploy("USDC", "USDC");
+    await usdc.deployed();
+
+    expect(await usdc.name()).to.equal("USDC");
+  });
+
+  it("Create a test NFT token", async function () {
     const TestNft = await ethers.getContractFactory("TestNFT");
     testNft = await TestNft.deploy("Test NFT", "Test NFT");
     await testNft.deployed();
@@ -15,16 +24,25 @@ describe("NFTLender", function () {
     // console.log(`Owner of 0: ${await testNFT.ownerOf(0)}`);
   });
 
-  it("Should deploy NFTLender contract", async function () {
+  it("Deploy NFTLender contract", async function () {
     const NFTLender = await ethers.getContractFactory("NFTLender");
-    nftLender = await NFTLender.deploy();
+    nftLender = await NFTLender.deploy(usdc.address);
     await nftLender.deployed();
   });
 
-  it("Should start a loan", async function () {
+  it("Start a loan", async function () {
     await testNft.approve(nftLender.address, 0);
 
-    const startLoanTx = await nftLender.startLoan(testNft.address, 0, 100000);
+    const startLoanTx = await nftLender.startLoan(testNft.address, 0, 100000, 60 * 60 * 24 * 10);
     await startLoanTx.wait();
+  });
+
+  it("Fund the loan", async function () {
+    var amount = 70000;
+
+    await usdc.approve(nftLender.address, amount);
+
+    const fundLoanTx = await nftLender.fundLoan(0, amount);
+    await fundLoanTx.wait();
   });
 });
